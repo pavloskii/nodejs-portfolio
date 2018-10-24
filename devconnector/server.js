7d4587473037a336ecb97347ef4692f6a355eb69
Igor Pavloski <Igor.Pavloski@seavus.com>
2018-10-24 23:10:20 +0200

    Authentication added

--- devconnector/server.js
+++ devconnector/server.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
 
 const app = express();
+
+//Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //DB config
 const db = require("./config/keys").mongoURI;
@@ -16,7 +22,10 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello"));
+// Passport middleware
+app.use(passport.initialize());
+//Passport config
+require("./config/passport")(passport);
 
 // API Routes
 app.use("/api/users", users);
@@ -26,3 +35,5 @@ app.use("/api/posts", posts);
 const port = process.env.PORT || 5000; //env.PORT for deploying to HEROKU
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
+
+// app.get("/", (req, res) => res.send("Hello"));
