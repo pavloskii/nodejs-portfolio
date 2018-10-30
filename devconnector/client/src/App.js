8dc53569d04ec4c17909426a50b1315568640b15
Igor Pavloski <Igor.Pavloski@seavus.com>
2018-10-30 22:38:20 +0100

    38

--- devconnector/client/src/App.js
+++ devconnector/client/src/App.js
@@ -1,12 +1,27 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Landing from "./components/layout/Landing";
 import "./App.css";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>My React App</h1>
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route path="/" component={Landing} exact />
+          <div className="container">
+            <Route path="/register" component={Register} exact />
+            <Route path="/login" component={Login} exact />
+          </div>
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
