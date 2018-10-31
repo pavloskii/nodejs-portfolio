5957ee71bab83a217f56ae88321173d9e214955e
Igor Pavloski <Igor.Pavloski@seavus.com>
2018-10-31 23:27:36 +0100

    40

--- devconnector/client/src/App.js
+++ devconnector/client/src/App.js
@@ -1,27 +1,31 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store/store";
 
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
-import "./App.css";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Route path="/" component={Landing} exact />
-          <div className="container">
-            <Route path="/register" component={Register} exact />
-            <Route path="/login" component={Login} exact />
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route path="/" component={Landing} exact />
+            <div className="container">
+              <Route path="/register" component={Register} exact />
+              <Route path="/login" component={Login} exact />
+            </div>
+            <Footer />
           </div>
-          <Footer />
-        </div>
-      </Router>
+        </Router>
+      </Provider>
     );
   }
 }
